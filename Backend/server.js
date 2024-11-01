1d347f04b6030b0e5ad5e44125964c49d5181c40
pratheesh18 <pratheesh1218@gmail.com>
2024-11-01 09:41:06 +0530

    refactored

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ connectDatabase();
 
 const app = express();
 app.use(cors({
-    origin:'https://recipe-application-client.vercel.app', // Update this to match your frontend URL
+    origin:['https://recipe-application-client.vercel.app','http://localhost:5173'], // Update this to match your frontend URL
     credentials: true,               // Allows cookies and credentials to be sent
 }));
 app.use(express.json());
